3cf2000aca95fc8933347821e1ab131f8aa884ad
COANelmejor <coanelmejor@gmail.com>
2023-02-26 03:42:42 -0600

    Nomalized idents

--- ShowHand.cs
+++ ShowHand.cs
@@ -1,40 +1,40 @@
 ﻿using System;
 namespace ConsoleBlackjack
 {
-	public class ShowHand
-	{
-		public static string Write(string[] hand, bool isASCIIArt, bool shouldBeBigStyle)
-		{
-			if (!isASCIIArt) {
-				return $"| { lang.infoHand}: {string.Join(" ", hand)}";
-			} else {
-				if (shouldBeBigStyle) {
-					return bigASCIIArt(hand);
+    public class ShowHand
+    {
+        public static string Write(string[] hand, bool isASCIIArt, bool shouldBeBigStyle)
+        {
+            if (!isASCIIArt) {
+                return $"| { lang.infoHand}: {string.Join(" ", hand)}";
+            } else {
+                if (shouldBeBigStyle) {
+                    return bigASCIIArt(hand);
                 } else {
-					return shortASCIIStyle(hand);
+                    return shortASCIIStyle(hand);
                 }
             }
-		}
+        }
 
-		static string bigASCIIArt(string[] hand)
-		{
-			return string.Join(" ", hand);
-		}
+        static string bigASCIIArt(string[] hand)
+        {
+            return string.Join(" ", hand);
+        }
 
-		static string shortASCIIStyle(string[] hand) {
+        static string shortASCIIStyle(string[] hand) {
             string line1 = string.Empty,
                    line2 = string.Empty,
                    line3 = string.Empty,
                    line4 = string.Empty;
 
-			for (int i = 0; i < hand.Length; i++) {
-				string card = hand[i];
-				string cardValue = card.Length == 2 ? $"{card.Substring(0, card.Length - 1)} " : card.Substring(0, card.Length - 1);
-				string cardSymbol = card.Substring(card.Length - 1);
+            for (int i = 0; i < hand.Length; i++) {
+                string card = hand[i];
+                string cardValue = card.Length == 2 ? $"{card.Substring(0, card.Length - 1)} " : card.Substring(0, card.Length - 1);
+                string cardSymbol = card.Substring(card.Length - 1);
                 line1 +=  "┌──┐";
-				line2 += $"│{cardValue}│";
-				line3 += $"│ {cardSymbol}│";
-				line4 +=  "└──┘";
+                line2 += $"│{cardValue}│";
+                line3 += $"│ {cardSymbol}│";
+                line4 +=  "└──┘";
             }
             
             return $"\n{line1}\n{line2}\n{line3}\n{line4}";
